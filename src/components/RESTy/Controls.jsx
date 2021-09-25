d4da32e985e884d00e0afca65926ce77aefb7813
Sofia Tejada <sofianais@icloud.com>
2021-09-25 15:21:16 -0700

    form

--- src/components/RESTy/Controls.jsx
+++ src/components/RESTy/Controls.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Controls() {
+export default function Controls({ handleChange, handleSubmit }) {
   return (
-    <div>
-      
-    </div>
+    <form onChange={handleChange} onSubmit={handleSubmi}>
+      <input aria-label="url" placeholder="URL" type="text" />
+      <section>
+        <label htmlFor="GET">
+          GET
+          <input id="get" value="GET" name="method" type="radio" />
+        </label>
+        <label htmlFor="POST">
+          POST
+          <input id="post" value="POST" name="method" type="radio" />
+        </label>
+        <label htmlFor="PUT">
+          PUT
+          <input id="put" value="PUT" name="method" type="radio" />
+        </label>
+        <label htmlFor="DELETE">
+          DELETE
+          <input id="delete" value="DELETE" name="method" type="radio" />
+        </label>
+      </section>
+    </form>
   );
 }
