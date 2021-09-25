fc4bbef9764b81368145d9e930d1838bc5e49c24
Sofia Tejada <sofianais@icloud.com>
2021-09-25 15:30:34 -0700

    controls.jsx

--- src/components/RESTy/Controls.jsx
+++ src/components/RESTy/Controls.jsx
@@ -3,26 +3,46 @@ import PropTypes from 'prop-types';
 
 export default function Controls({ handleChange, handleSubmit }) {
   return (
-    <form onChange={handleChange} onSubmit={handleSubmi}>
+    <form onChange={handleChange} onSubmit={handleSubmit}>
+
       <input aria-label="url" placeholder="URL" type="text" />
+
       <section>
+
         <label htmlFor="GET">
           GET
           <input id="get" value="GET" name="method" type="radio" />
         </label>
+
         <label htmlFor="POST">
           POST
           <input id="post" value="POST" name="method" type="radio" />
         </label>
+
         <label htmlFor="PUT">
           PUT
           <input id="put" value="PUT" name="method" type="radio" />
         </label>
+
         <label htmlFor="DELETE">
           DELETE
           <input id="delete" value="DELETE" name="method" type="radio" />
         </label>
+
+        <button aria-label="submit-button">Submit</button>
       </section>
+
+      <section>
+
+        <textarea aria-label="data" name="data" placeholder="JSON Body" />
+
+      </section>
+
     </form>
   );
 }
+
+Controls.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
