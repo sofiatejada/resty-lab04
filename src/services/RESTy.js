f13d67b571a88b0617effb3b208b8e3cfcfb5735
Sofia Tejada <sofianais@icloud.com>
2021-09-28 17:24:36 -0700

    finished

--- src/services/RESTy.js
+++ src/services/RESTy.js
@@ -1,19 +1,19 @@
 // import request from 'superagent';
 
 // export default async function restyCalls(method, url, data) {
-//   if(method === 'GET') {
+//   if(method == 'GET') {
 //     const { body } = await request.get(url);
 //     return body;
 //   }
-//   if(method === 'POST') {
-//     const { body } = await request.post(url).set(data);
+//   if(method == 'POST') {
+//     const { body } = await request.post(url).send(data);
 //     return body;
 //   } 
-//   if(method === 'PUT') {
+//   if(method == 'PUT') {
 //     const { body } = await request.put(url).send(data);
 //     return body;
 //   }
-//   if(method === 'DELETE') {
+//   if(method == 'DELETE') {
 //     const { body } = await request.delete(url);
 //     return body;
 //   }
@@ -26,7 +26,7 @@ export const restyCalls = (method, url, data) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      data,
+      body: data,
     }).then((res) => res.json());
   } else {
     return fetch(url, { method }).then((res) => res.json());
